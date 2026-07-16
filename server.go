f3b6a0e9ec4704d65f065a9ed3e4a4249e83b0c5
fOrOn-O <2304300203@wtu.edu.cn>
2026-07-16 19:32:12 +0800

    feat: 完善聊天室功能
    - 实现用户上下线广播功能
    - 添加 User 结构体和消息监听机制
    - 实现服务器消息分发到各用户通道

--- server.go
+++ server.go
@@ -3,39 +3,79 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	Message chan string
 }
 
 // 创建一个server的接口
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
-func (this *Server) Handler (conn net.Conn){
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+
+	this.Message <- sendMsg
+}
+
+// 监听message
+func (this *Server) ListenMessage() {
+	for {
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) Handler(conn net.Conn) {
 	fmt.Println("链接建立成功!")
+	user := NewUser(conn)
+	//加入用户到onlinemap
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+	//广播当前用户
+	this.BroadCast(user, "已上线")
+
+	//阻塞handler
+	select {}
+
 }
 
 // 启动服务器的接口
 func (this *Server) Start() {
-	listener, err := net.Listen("tcp",fmt.Sprintf("%s:%d",this.Ip,this.Port))
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
 	if err != nil {
-		fmt.Println("net.Lesten err:",err)
+		fmt.Println("net.Lesten err:", err)
 		return
 	}
 	defer listener.Close()
 
+	go this.ListenMessage()
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			fmt.Println("listener accept err:",err)
+			fmt.Println("listener accept err:", err)
 			continue
 		}
 
